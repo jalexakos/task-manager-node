ec93f4e15305a3a659912379cc751fe855c8c0eb
jalexakos <josiah.alexakos@gmail.com>
2021-02-05 13:50:14 -0500

    Further updates - routing

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 Learning Node.JS by building a full CRUD application - Task Manager.
 
-From "The Complete Node.js Developer Course" by Andrew Mead on Udemy (found here: https://www.udemy.com/course/the-complete-nodejs-developer-course-2/).
+From "The Complete Node.js Developer Course" by Andrew Mead on Udemy ([found here](https://www.udemy.com/course/the-complete-nodejs-developer-course-2/)).
+
+## Quick Tips
+
+Upon forking, run `npm install` to install the requisite dependencies.
+
+To start a development server using Nodemon, run `npm run dev`. This allows for live updating and developing on localhost.
---
+++ src/routers/task.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const Task = require('../models/task');
+const router = new express.Router();
+
+// Tasks Post Endpoint
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+    try{
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// Tasks Get Endpoint
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Task Get Endpoint (Specific Task)
+router.get('/tasks/:id', async (req, res) => {
+    const _id = req.params.id
+    try {
+        const task = await Task.findById(_id)
+        if (!task) {
+            return res.status(404).send()
+        }
+    
+        res.send(task)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+// Task Update Endpoint (Specific Task)
+router.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch (e) {
+        if (e.errors.description.name && e.errors.description.name === "ValidatorError") {
+            return res.status(400).send(e.errors.description.message)
+        }
+        return res.status(400).send(e)
+    }
+})
+
+// Task Delete Endpoint (Specific Task)
+router.delete('/tasks/:id', async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(e) {
+        res.status(500).send()
+    }
+})
+
+module.exports = router
